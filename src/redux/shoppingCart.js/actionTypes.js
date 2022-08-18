4b7f2b13876253ac03bc19d7b7a7278e00478e96
Tohidul Islam <mdtohid.sust@gmail.com>
2022-08-18 13:30:19 +0600

    shopping_cart reducer implemented

--- src/redux/shoppingCart.js/actionTypes.js
+++ src/redux/shoppingCart.js/actionTypes.js
@@ -1,5 +1,5 @@
 export const ADD_TO_CART = "shoppingCart/add_to_cart";
 export const REMOVE_FROM_CART = "shoppingCart/remove_from_cart";
+export const SET_QUANTITY = "shoppingCart/set_quantity";
 export const INCREASE_QUANTITY = "shoppingCart/increase_quantity";
 export const DECREASE_QUANTITY = "shoppingCart/decrease_quantity";
-export const SET_QUANTITY = "shoppingCart/set_quantity";
--- src/redux/shoppingCart.js/shoppingCartReducer.js
+++ src/redux/shoppingCart.js/shoppingCartReducer.js
@@ -1,27 +1,80 @@
 import {
   ADD_TO_CART,
-  DECREASE_QUANTITY,
-  INCREASE_QUANTITY,
   REMOVE_FROM_CART,
   SET_QUANTITY,
+  DECREASE_QUANTITY,
+  INCREASE_QUANTITY,
 } from "./actionTypes.js";
 
-const initialState = [];
+const initialState = {
+  cartItems: [],
+};
 
 const shoppingCartReducer = (state = initialState, action) => {
   const { type, payload } = action;
+  const { cartItems } = state;
 
   switch (type) {
     case ADD_TO_CART:
-      break;
+      const inCart = cartItems.find((cartItem) => {
+        return cartItem.id === payload.id;
+      });
+
+      return {
+        ...state,
+        cartItems: inCart
+          ? cartItems.map((cartItem) => {
+              return cartItem.id === payload.id
+                ? {
+                    ...cartItem,
+                    quantity: cartItem.quantity + 1,
+                    stock: cartItem.stock && cartItem.stock - 1,
+                  }
+                : cartItem;
+            })
+          : [
+              ...cartItems,
+              {
+                ...payload,
+                quantity: 1,
+                stock: cartItem.stock && cartItem.stock - 1,
+              },
+            ],
+      };
+
     case REMOVE_FROM_CART:
-      break;
-    case INCREASE_QUANTITY:
-      break;
-    case DECREASE_QUANTITY:
-      break;
+      return {
+        ...state,
+        cartItems: cartItems.filter((cartItem) => cartItem.id !== payload.id),
+      };
+
     case SET_QUANTITY:
-      break;
+      return {
+        ...state,
+        cartItems: cartItems.map((cartItem) =>
+          cartItem.id === payload.id
+            ? { ...cartItem, quantity: payload.quantity }
+            : cartItem
+        ),
+      };
+    case INCREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: cartItems.map((cartItem) =>
+          cartItem.id === payload.id
+            ? { ...cartItem, quantity: cartItem + 1 }
+            : cartItem
+        ),
+      };
+    case DECREASE_QUANTITY:
+      return {
+        ...state,
+        cartItems: cartItems.map((cartItem) =>
+          cartItem.id === payload.id
+            ? { ...cartItem, quantity: cartItem - 1 }
+            : cartItem
+        ),
+      };
 
     default:
       return state;
